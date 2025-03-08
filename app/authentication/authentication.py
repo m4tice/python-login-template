7d665aa5f9531309d9b3c9819c6560525d31ec76
m4tice <ductuan2908@gmail.com>
2025-03-08 19:46:47 +0700

    update: create routes for authentication use cases

--- app/authentication/authentication.py
+++ app/authentication/authentication.py
@@ -16,13 +16,27 @@ def login():
 @authentication_bp.route('/loginv2')
 def loginv2():
     """
-    route: /login
+    route: /auth/login
     """
     return render_template('authentication/loginv2.html')
 
 @authentication_bp.route('/login/<username>/<password>', methods=['GET'])
 def authenticate(username, password):
     """
-    route: /login/<username>/<password>
+    route: /auth/login/<username>/<password>
     """
     return jsonify({'result': True})
+
+@authentication_bp.route('/registration')
+def registration():
+    """
+    route: /auth/registration
+    """
+    return render_template('authentication/registration.html')
+
+@authentication_bp.route('/restore-password')
+def restore_password():
+    """
+    route: /auth/restore-password
+    """
+    return render_template('authentication/restore-password.html')
--- app/authentication/static/login.js
+++ app/authentication/static/login.js
@@ -27,20 +27,30 @@ function authenticate(username, password) {
 }
 
 
-document.getElementById('button-register').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent form submission
-    const username = document.querySelector('input[type="text"]').value;
-    const password = document.querySelector('input[type="password"]').value;
-    register(username, password)
+// document.getElementById('button-register').addEventListener('click', function(event) {
+//     event.preventDefault(); // Prevent form submission
+//     const username = document.querySelector('input[type="text"]').value;
+//     const password = document.querySelector('input[type="password"]').value;
+//     register(username, password)
+// });
+
+// function register(username, password) {
+//     fetch(`/auth/register/${username}/${password}`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+// }
+
+document.querySelector('.register a').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = '/auth/registration'; // Redirect to home page
 });
 
-function register(username, password) {
-    fetch(`/auth/register/${username}/${password}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-}
+document.querySelector('.forget a').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = '/auth/restore-password'; // Redirect to home page
+});
