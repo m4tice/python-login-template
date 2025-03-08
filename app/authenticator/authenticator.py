90aa05483708b889f077b36f9f6e9f1194f02e96
m4tice <ductuan2908@gmail.com>
2025-03-09 00:15:43 +0700

    update: authenticator implementation

---
+++ app/authenticator/authenticator.py
@@ -0,0 +1,19 @@
+"""
+author: @GUU8HC
+"""
+
+class Authenticator:
+    """
+    Authenticator class
+    """
+    def __init__(self, db):
+        """
+        *.db initialization
+        """
+        self.db = db
+
+    def authenticate(self, username, password):
+        """
+        Authenticate user
+        """
+        return False
