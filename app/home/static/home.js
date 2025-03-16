ae4c7230f8df41ba148fe744154431cc0f59fe64
m4tice <ductuan2908@gmail.com>
2025-03-16 18:04:34 +0700

    create: navbar for public homepage

--- app/home/static/home.js
+++ app/home/static/home.js
@@ -1,7 +1,7 @@
 document.getElementById('button-signout').addEventListener('click', function(event) {
     event.preventDefault();
     signout();
-})
+});
 
 function signout(){
     window.location.href = '/auth/signin';
